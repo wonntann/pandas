3f98febd0df667dba769cb75a0cd0183033ef6f4
Tan <atpulley@tutanota.com>
2021-05-27 22:31:58 -0700

    first commit

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ df = pd.read_csv('diamonds.csv', index_col=0)
 df.head()
 
 # https://www.youtube.com/watch?v=BpPJxtOk8uw&ab_channel=sentdex
+
+
+# https://www.youtube.com/watch?v=LHBE6Q9XlzI&ab_channel=freeCodeCamp.org
